433bd76b3f9be071ccdc8fb91de28dbcb7083921
bhattsankalp <sankalpbhatt123@gmail.com>
2019-09-29 15:25:27 +0530

    added user_accounts table scripts

---
+++ Database/Script/user_accounts.sql
@@ -0,0 +1,13 @@
+
+
+
+CREATE TABLE user_accounts
+(
+user_id varchar(45) NOT NULL PRIMARY KEY,
+user_password varchar(45) NOT NULL
+);
+
+
+insert into user_accounts (user_id, user_password) values ('dheeraj','dheeraj');
+insert into user_accounts (user_id, user_password) values ('dheeraj','dheeraj');
+
